bdc82568b6717adef8ed760674aeb42d25e588fe
Michael P <michael@mcmoo.org>
2022-12-30 12:28:06 -0500

    p345: optimize

--- src/p345.rs
+++ src/p345.rs
@@ -1,16 +1,35 @@
 struct Solution;
 impl Solution {
+    #[inline]
+    fn is_vowel(ch: char) -> bool {
+        match ch.to_ascii_lowercase() {
+            'a' | 'e' | 'i' | 'o' | 'u' => true,
+            _ => false
+        }
+    }
+
     pub fn reverse_vowels(mut s: String) -> String {
-        // collect all vowels and their indices
-        let (indices, vowels): (Vec<_>, Vec<_>) = s.char_indices()
-            .filter(|(_i, ch)| "aeiou".contains(ch.to_ascii_lowercase()))
-            .unzip();
-        vowels.into_iter()
-            .rev()
-            .zip(indices)
-            .for_each(|(new_vowel, index)| {
-                s.replace_range(index..(index+1), &new_vowel.to_string());
-            });
+        let mut start = 0usize;
+        let mut end = s.len();
+        while start < end {
+            end -= 1;
+            while end > start && !Solution::is_vowel(s.as_bytes()[end].to_ascii_lowercase().into()) {
+                end -= 1;
+            }
+            while start < end && !Solution::is_vowel(s.as_bytes()[start].to_ascii_lowercase().into()) {
+                start += 1;
+            }
+            if start >= end {
+                return s;
+            }
+            unsafe {
+                let bytes = s.as_bytes_mut();
+                let byte = bytes[start];
+                bytes[start] = bytes[end];
+                bytes[end] = byte;
+            }
+            start += 1;
+        }
         s
     }
 }
@@ -23,5 +42,7 @@ mod tests {
     fn samples() {
         assert_eq!(Solution::reverse_vowels("leetcode".to_owned()), "leotcede");
         assert_eq!(Solution::reverse_vowels("BASEMENT".to_owned()), "BESEMANT");
+        assert_eq!(Solution::reverse_vowels("".to_owned()), "");
+        assert_eq!(Solution::reverse_vowels("AEII".to_owned()), "IIEA");
     }
 }
