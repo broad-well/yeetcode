6abc4f455a620a4071008489df5180250e23824e
Michael P <michael@mcmoo.org>
2022-12-30 19:30:02 -0500

    p1025: spoiler

--- src/p1025.rs
+++ src/p1025.rs
@@ -1,30 +1,7 @@
 struct Solution;
 impl Solution {
     pub fn divisor_game(n: i32) -> bool {
-        let mut memo: Vec<bool> = Vec::with_capacity(n as usize);
-        // 1 -> Alice loses
-        memo.push(false);
-        // 2 -> Alice wins
-        memo.push(true);
-
-        while memo.len() < n as usize {
-            let base_num = memo.len() + 1;
-            let winning = (1..(base_num / 2))
-                .filter(|x| base_num % x == 0)
-                .any(|x| !memo[memo.len() - x]);
-            memo.push(winning);
-        }
-
-        memo[n as usize - 1]
-        // 2: A1, n=1 true
-        // 3: A1, B1 n=1 false
-        // 4: A1 -> B starts with 3 B1, A1, true
-        // 5: A1, B starts with 4, false
-        // 6: (choose 1: B starts with 5, true)
-        // 7: (choose 1: B starts with 6, false)
-        // 8: (choose 1: B starts with 7, true)
-        // 9: ()
-        // if you can choose 1 or 2, then you can probably win
+        n % 2 == 0
     }
 }
 
