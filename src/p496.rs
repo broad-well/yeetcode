9973fa1a47ee6185aa6aad6b704af68ba859fd19
Michael P <michael@mcmoo.org>
2023-01-01 21:59:56 -0500

    p496: optimize the old approach

--- src/p496.rs
+++ src/p496.rs
@@ -2,39 +2,20 @@
 
 struct Solution;
 impl Solution {
-    // premature optimization did not go well?
-    // pub fn next_greater_element_btree(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-    //     use std::collections::{HashMap, BTreeSet};
-    //     let mut result_mapping: HashMap<i32, i32> = HashMap::new();
-    //     let mut candidates: BTreeSet<i32> = BTreeSet::new();
-    //     for num in nums2.into_iter().rev() {
-    //         if let Some(answer) = candidates.range((num + 1)..).next() {
-    //             result_mapping.insert(num, *answer);
-    //         }
-    //         candidates.retain(|cand| *cand > num);
-    //         candidates.insert(num);
-    //     }
-    //     nums1.into_iter()
-    //         .map(|n| *result_mapping.get(&n).unwrap_or(&-1))
-    //         .collect()
-    // }
-
     pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-        use std::collections::HashMap;
-        let mut staging_area: Vec<i32> = Vec::with_capacity(nums1.len()/2);
-        let mut results: HashMap<i32, i32> = HashMap::new();
-        for n in &nums1 {
-            results.insert(*n, -1);
-        }
-        for n in nums2 {
-            while !staging_area.is_empty() && n > *staging_area.last().unwrap() {
-                results.insert(staging_area.pop().unwrap(), n);
+        use std::collections::{HashMap, BTreeSet};
+        let mut result_mapping: HashMap<i32, i32> = HashMap::new();
+        let mut candidates: BTreeSet<i32> = BTreeSet::new();
+        for num in nums2.into_iter().rev() {
+            candidates.retain(|cand| *cand > num);
+            if let Some(answer) = candidates.iter().next() {
+                result_mapping.insert(num, *answer);
             }
-            if results.contains_key(&n) {
-                staging_area.push(n);
-            }
+            candidates.insert(num);
         }
-        nums1.into_iter().map(|x| results[&x]).collect()
+        nums1.into_iter()
+            .map(|n| *result_mapping.get(&n).unwrap_or(&-1))
+            .collect()
     }
 }
 
